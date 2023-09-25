581528cd2b8d25e708a00c71fc8f100a82d7d55b
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2023-09-25 18:05:37 +0700

    api signup

--- SneakerStore/AppApi/Program.cs
+++ SneakerStore/AppApi/Program.cs
@@ -1,3 +1,4 @@
+using AppApi;
 using AppApi.SneakerDbContext;
 using AppData.Models;
 using Microsoft.AspNetCore.Identity;
@@ -11,10 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>().AddEntityFrameworkStores<SNDbcontext>().AddDefaultTokenProviders();
+
 builder.Services.AddDbContext<SNDbcontext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SneakerStore"));
 });
+
+ServiceRegistration.Configure(builder.Services);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
