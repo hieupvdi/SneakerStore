7043efcc79a5befffd5f8df22360bd928521556c
DESKTOP-2V5BKJA\Admin <hieuphamvan.dev.it@gmail.com>
2023-10-12 19:49:23 +0700

    SanPhamRepository

--- SneakerStore/AppApi/IRepositories/IAnhSanPhamRepository.cs
+++ SneakerStore/AppApi/IRepositories/IAnhSanPhamRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace AppApi.IRepositories
+﻿using AppData.Models;
+
+namespace AppApi.IRepositories
 {
     public interface IAnhSanPhamRepository
     {
+        public Task<List<AnhSanPham>> GetAll();
+        public Task<Guid> Create(AnhSanPham asp);
+        public Task<Guid> Edit(AnhSanPham asp);
+        public Task<int> Delete(Guid id);
+        public Task<AnhSanPham> GetById(Guid id);
     }
 }
