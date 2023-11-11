72d43c1982a6998f7ac137ce81bbf70473dc86bd
DESKTOP-2V5BKJA\Admin <hieuphamvan.dev.it@gmail.com>
2023-11-11 08:18:02 +0700

    add thong ke

--- SneakerStore/SneakerStore/Areas/Admin/Controllers/ThongKeController.cs
+++ SneakerStore/SneakerStore/Areas/Admin/Controllers/ThongKeController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppData.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace SneakerStore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ThongKeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> ThongKe(string tu_ngay, string den_ngay)
         {
-            return View();
+            DateTime? fromDate = null;
+            DateTime? toDate = null;
+
+            if (!string.IsNullOrWhiteSpace(tu_ngay) && DateTime.TryParse(tu_ngay, out var fromDateParsed))
+            {
+                fromDate = fromDateParsed;
+            }
+
+            if (!string.IsNullOrWhiteSpace(den_ngay) && DateTime.TryParse(den_ngay, out var toDateParsed))
+            {
+                // Add one day to include the entire end day
+                toDate = toDateParsed.AddDays(1);
+            }
+
+            var httpClient = new HttpClient();
+            string apiURL = "https://localhost:7001/api/HoaDon/HoaDon/get-all";
+            var response = await httpClient.GetAsync(apiURL);
+            string apiData = await response.Content.ReadAsStringAsync();
+            var allData = JsonConvert.DeserializeObject<List<HoaDonVM>>(apiData);
+
+            // Filter the data based on the date range
+            var filteredData = allData
+                .Where(item => (!fromDate.HasValue || item.NgayTao >= fromDate)
+                    && (!toDate.HasValue || item.NgayTao < toDate))
+                .ToList();
+
+            return View(filteredData);
         }
+       
+
+
     }
 }
